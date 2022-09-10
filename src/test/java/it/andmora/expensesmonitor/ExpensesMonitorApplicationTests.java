3223ef695f880710d7f49bf0352cb107bf393f03
andmora <and.morabito@gmail.com>
2022-09-10 20:05:58 +0200

    Initial commit

---
+++ src/test/java/it/andmora/expensesmonitor/ExpensesMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package it.andmora.expensesmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
