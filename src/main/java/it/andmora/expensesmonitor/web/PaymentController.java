8b8a5357dd3233c9cd7ee9e6a91fb18842472c98
Andrea Morabito <andrea.morabito@pagopa.it>
2022-10-16 17:27:30 +0200

    [EM-20] implement rest

--- src/main/java/it/andmora/expensesmonitor/web/PaymentController.java
+++ src/main/java/it/andmora/expensesmonitor/web/PaymentController.java
@@ -1,9 +1,14 @@
 package it.andmora.expensesmonitor.web;
 
 import it.andmora.expensesmonitor.web.dto.PaymentDto;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("payment")
 public interface PaymentController {
 
-  PaymentDto createPayment(PaymentDto paymentDto);
+  @PutMapping("")
+  PaymentDto createPayment(@RequestBody PaymentDto paymentDto);
 
 }
