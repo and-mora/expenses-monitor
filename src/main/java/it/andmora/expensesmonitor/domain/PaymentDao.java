69818af585ff7a2ff717835ad10837b7c6292fb0
Andrea Morabito <andrea.morabito@pagopa.it>
2022-10-23 22:50:46 +0200

    [EM-18] creation of dao methods for balance use case

--- src/main/java/it/andmora/expensesmonitor/domain/PaymentDao.java
+++ src/main/java/it/andmora/expensesmonitor/domain/PaymentDao.java
@@ -1,10 +1,11 @@
 package it.andmora.expensesmonitor.domain;
 
 import it.andmora.expensesmonitor.domain.entity.Payment;
-import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 public interface PaymentDao {
 
   Mono<Payment> savePayment(Payment payment);
+
+  Mono<Integer> getOverallBalance();
 }
