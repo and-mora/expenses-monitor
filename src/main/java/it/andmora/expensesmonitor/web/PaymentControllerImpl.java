b5ee3e8d2687df45e5a473f2bb051ce75c957a1a
Andrea Morabito <78792023+and-mora@users.noreply.github.com>
2022-10-23 22:17:17 +0200

    [EM-20] implement rest controller (#11)

--- src/main/java/it/andmora/expensesmonitor/web/PaymentControllerImpl.java
+++ src/main/java/it/andmora/expensesmonitor/web/PaymentControllerImpl.java
@@ -1,14 +1,18 @@
 package it.andmora.expensesmonitor.web;
 
+import it.andmora.expensesmonitor.domain.usecase.PaymentCreator;
 import it.andmora.expensesmonitor.web.dto.PaymentDto;
 import it.andmora.expensesmonitor.web.mapper.PaymentMapper;
-import it.andmora.expensesmonitor.domain.usecase.PaymentCreator;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * The controller in this layer is the interface between the use cases and any outside interfaces
  * Here happens all the data format conversion from and to whatever is required by the use cases
  */
+@RestController
+@Slf4j
 @RequiredArgsConstructor
 class PaymentControllerImpl implements PaymentController {
 
@@ -17,6 +21,8 @@ class PaymentControllerImpl implements PaymentController {
 
   @Override
   public PaymentDto createPayment(PaymentDto paymentDto) {
-    return paymentMapper.entityToDto(paymentCreator.createPayment(paymentMapper.dtoToEntity(paymentDto)));
+    log.info("Creation of a new payment...");
+    return paymentMapper.entityToDto(
+        paymentCreator.createPayment(paymentMapper.dtoToEntity(paymentDto)));
   }
 }
--- src/test/java/it/andmora/expensesmonitor/web/PaymentControllerImplTest.java
+++ src/test/java/it/andmora/expensesmonitor/web/PaymentControllerImplTest.java
@@ -2,12 +2,16 @@ package it.andmora.expensesmonitor.web;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import it.andmora.expensesmonitor.web.dto.PaymentDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import it.andmora.expensesmonitor.domain.entity.Payment;
 import it.andmora.expensesmonitor.domain.entity.PaymentType;
-import it.andmora.expensesmonitor.web.mapper.PaymentMapper;
 import it.andmora.expensesmonitor.domain.usecase.PaymentCreator;
+import it.andmora.expensesmonitor.web.dto.PaymentDto;
+import it.andmora.expensesmonitor.web.mapper.PaymentMapper;
 import java.time.OffsetDateTime;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,17 +20,26 @@ import org.mapstruct.factory.Mappers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+@WebMvcTest(PaymentControllerImpl.class)
 class PaymentControllerImplTest {
-
 
   PaymentMapper paymentMapper = Mappers.getMapper(PaymentMapper.class);
   @Mock
   PaymentCreator paymentCreator;
   PaymentController paymentController;
-
   OffsetDateTime dateInjected = OffsetDateTime.now();
   AutoCloseable closeable;
+//  @Autowired
+//  MockMvc mockMvc;
+//
+//  private final String BASE_URI = "http://localhost:8080";
+//  private static final String PUT_PAYMENT_ENDPOINT = "/payment";
 
   @BeforeEach
   void setup() {
@@ -53,6 +66,25 @@ class PaymentControllerImplTest {
     assertThat(paymentResponse).extracting("accountingDate").isEqualTo(dateInjected);
     assertThat(paymentResponse).extracting("isIncomeVoice").isEqualTo(false);
   }
+
+//  @Test
+//  void whenRestInterfaceIsCalledThen200() throws Exception {
+//    Mockito.when(paymentCreator.createPayment(any())).thenReturn(createDefaultPayment());
+//    PaymentDto paymentDto = createPaymentDto();
+//    ObjectMapper mapper = new ObjectMapper();
+//    String paymentDtoAsJson = mapper.writeValueAsString(paymentDto);
+//
+//    mockMvc.perform(MockMvcRequestBuilders
+//            .put(BASE_URI + PUT_PAYMENT_ENDPOINT)
+//            .contentType(MediaType.APPLICATION_JSON)
+//            .content(paymentDtoAsJson))
+//        .andExpectAll(status().isOk(),
+//            content().contentType(MediaType.APPLICATION_JSON_VALUE),
+//            jsonPath("$.description").value("shopping"),
+//            jsonPath("$.merchantName").value("H&M"),
+//            jsonPath("$.amount").value(100)
+//        );
+//  }
 
   PaymentDto createPaymentDto() {
     return PaymentDto.builder()
