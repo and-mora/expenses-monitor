9ea2f7bbf9ec96f5ebae1ff5e3eb8ce68c5d4420
Andrea Morabito <78792023+and-mora@users.noreply.github.com>
2022-10-30 14:20:06 +0100

    [EM-26] implements the data layer with mongodb (#18)

---
+++ src/test/java/it/andmora/expensesmonitor/dao/PaymentDaoImplTest.java
@@ -0,0 +1,103 @@
+package it.andmora.expensesmonitor.dao;
+
+import it.andmora.expensesmonitor.dao.dbmodel.PaymentDbEntity;
+import it.andmora.expensesmonitor.dao.mapper.PaymentDbMapper;
+import it.andmora.expensesmonitor.dao.persistance.PaymentMongoRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+class PaymentDaoImplTest {
+
+  @Mock
+  PaymentMongoRepository repository;
+  PaymentDaoImpl paymentDao;
+  AutoCloseable autoCloseable;
+
+  @BeforeEach
+  void setup() {
+    autoCloseable = MockitoAnnotations.openMocks(this);
+    paymentDao = new PaymentDaoImpl(repository, Mappers.getMapper(PaymentDbMapper.class));
+  }
+
+  @AfterEach
+  void cleanup() throws Exception {
+    autoCloseable.close();
+  }
+
+  @Test
+  void givenTwoOutcomePaymentsWhenGetOverallBalanceThenSum() {
+    // given
+    Mockito.when(repository.findAll()).thenReturn(createOutcomeResponse());
+
+    // when
+    Mono<Integer> overallBalance = paymentDao.getOverallBalance();
+
+    // then
+    StepVerifier
+        .create(overallBalance)
+        .expectNext(-400)
+        .expectComplete()
+        .verify();
+  }
+
+  @Test
+  void givenTwoMixedPaymentsWhenGetOverallBalanceThenSum() {
+    // given
+    Mockito.when(repository.findAll()).thenReturn(createMixedResponse());
+
+    // when
+    Mono<Integer> overallBalance = paymentDao.getOverallBalance();
+
+    // then
+    StepVerifier
+        .create(overallBalance)
+        .expectNext(-200)
+        .expectComplete()
+        .verify();
+  }
+
+  @Test
+  void givenTwoIncomePaymentsWhenGetOverallBalanceThenSum() {
+    // given
+    Mockito.when(repository.findAll()).thenReturn(createIncomeResponse());
+
+    // when
+    Mono<Integer> overallBalance = paymentDao.getOverallBalance();
+
+    // then
+    StepVerifier
+        .create(overallBalance)
+        .expectNext(500)
+        .expectComplete()
+        .verify();
+  }
+
+  Flux<PaymentDbEntity> createOutcomeResponse() {
+    return Flux.just(PaymentDbEntity.builder().id("1").paymentType("OUTCOME").amount(100).build(),
+        PaymentDbEntity.builder().id("2").paymentType("OUTCOME").amount(300).build());
+  }
+
+  Flux<PaymentDbEntity> createMixedResponse() {
+    return Flux.just(PaymentDbEntity.builder().id("1").paymentType("INCOME").amount(100).build(),
+        PaymentDbEntity.builder().id("2").paymentType("OUTCOME").amount(300).build());
+  }
+
+  Flux<PaymentDbEntity> createIncomeResponse() {
+    return Flux.just(PaymentDbEntity.builder().id("1").paymentType("INCOME").amount(200).build(),
+        PaymentDbEntity.builder().id("2").paymentType("INCOME").amount(300).build());
+  }
+
+//  Flux<PaymentDbEntity> createNegativeResponse() {
+//    return Flux.just(PaymentDbEntity.builder().id("1").paymentType("INCOME").amount(100).build(),
+//        PaymentDbEntity.builder().id("2").paymentType("OUTCOME").amount(300).build());
+//  }
+
+}
---
+++ src/test/java/it/andmora/expensesmonitor/dao/mapper/PaymentDbMapperTest.java
@@ -0,0 +1,57 @@
+package it.andmora.expensesmonitor.dao.mapper;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import it.andmora.expensesmonitor.dao.dbmodel.PaymentDbEntity;
+import it.andmora.expensesmonitor.domain.entity.Payment;
+import it.andmora.expensesmonitor.domain.entity.PaymentType;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+
+class PaymentDbMapperTest {
+
+  LocalDateTime dateInjected = LocalDateTime.now();
+  PaymentDbMapper mapper = Mappers.getMapper(PaymentDbMapper.class);
+
+  @Test
+  void whenMapFromDomainToDbEntityThenOk() {
+    Payment payment = mapper.dbEntityToDomain(createDefaultEntity());
+
+    assertThat(payment).extracting("description").isEqualTo("shopping");
+    assertThat(payment).extracting("merchantName").isEqualTo("H&M");
+    assertThat(payment).extracting("amount").isEqualTo(1000);
+    assertThat(payment).extracting("accountingDate").isEqualTo(dateInjected);
+    assertThat(payment).extracting("paymentType").isEqualTo(PaymentType.OUTCOME);
+  }
+
+  @Test
+  void whenMapFromDbEntityToDomainThenOk() {
+    PaymentDbEntity payment = mapper.domainToDbEntity(createDefaultPayment());
+
+    assertThat(payment).extracting("description").isEqualTo("shopping");
+    assertThat(payment).extracting("merchantName").isEqualTo("H&M");
+    assertThat(payment).extracting("amount").isEqualTo(1000);
+    assertThat(payment).extracting("accountingDate").isEqualTo(dateInjected);
+    assertThat(payment).extracting("paymentType").isEqualTo("OUTCOME");
+  }
+
+  Payment createDefaultPayment() {
+    return Payment.builder()
+        .description("shopping")
+        .merchantName("H&M")
+        .amount(1000)
+        .accountingDate(dateInjected)
+        .paymentType(PaymentType.OUTCOME).build();
+  }
+
+  PaymentDbEntity createDefaultEntity() {
+    return PaymentDbEntity.builder()
+        .description("shopping")
+        .merchantName("H&M")
+        .amount(1000)
+        .accountingDate(dateInjected)
+        .paymentType("OUTCOME").build();
+  }
+
+}
