6a0c69a8ec53d5d338a53f1cc821faf8815ee23e
Andrea Morabito <and.morabito@gmail.com>
2023-05-07 14:35:09 +0200

    [EM-44] fix repository query

--- src/main/java/it/andmora/expensesmonitor/dao/persistance/ReportMongoRepository.java
+++ src/main/java/it/andmora/expensesmonitor/dao/persistance/ReportMongoRepository.java
@@ -2,11 +2,13 @@ package it.andmora.expensesmonitor.dao.persistance;
 
 import it.andmora.expensesmonitor.dao.dbmodel.PaymentDbEntity;
 import java.time.LocalDateTime;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
 
 public interface ReportMongoRepository extends ReactiveMongoRepository<PaymentDbEntity, String> {
 
-  Flux<PaymentDbEntity> findByAccountingDateGreaterThanAndAccountingDateLessThan(
-      LocalDateTime startDate, LocalDateTime endDate);
+  // between works as exclusive, need to explicit query for inclusive start/end
+  @Query("{'accountingDate' : { $gte: ?0, $lte: ?1 } }")
+  Flux<PaymentDbEntity> findByAccountingDateBetween(LocalDateTime startDate, LocalDateTime endDate);
 }
