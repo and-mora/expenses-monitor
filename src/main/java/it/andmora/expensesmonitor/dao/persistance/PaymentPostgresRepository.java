5aca96c6c0a5f6f16d89e0e8809fe1167c0ce1c9
Andrea Morabito <andrea.morabito@pagopa.it>
2023-08-21 12:45:39 +0200

    [EM-51] change repository

--- src/main/java/it/andmora/expensesmonitor/dao/persistance/PaymentPostgresRepository.java
+++ src/main/java/it/andmora/expensesmonitor/dao/persistance/PaymentPostgresRepository.java
@@ -1,11 +1,11 @@
 package it.andmora.expensesmonitor.dao.persistance;
 
 import it.andmora.expensesmonitor.dao.dbmodel.PaymentDbEntity;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 /**
  * The persistance layer implements the repository or anything needed to persist the domain objects
  */
-public interface PaymentMongoRepository extends ReactiveMongoRepository<PaymentDbEntity, String> {
+public interface PaymentPostgresRepository extends ReactiveCrudRepository<PaymentDbEntity, Integer> {
 
 }
