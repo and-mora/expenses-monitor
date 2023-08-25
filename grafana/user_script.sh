aac7f8a5a523c699c47d17eb1b3a4f507e15bb38
Andrea Morabito <78792023+and-mora@users.noreply.github.com>
2023-08-25 18:04:44 +0200

    fix: [EM-77] fix protocol and host in grafana init script (#37)

--- grafana/user_script.sh
+++ grafana/user_script.sh
@@ -18,25 +18,25 @@ curl -X POST -H "Content-Type: application/json" -d "{
   \"email\":\"and.morabito@gmail.com\",
   \"login\":\"andrea\",
   \"password\":\"$USER_PASSWORD\"
-}" http://admin:admin@localhost:3000/api/admin/users
+}" https://admin:admin@expmonitor.freeddns.org:3000/api/admin/users
 
-#curl  http://admin:admin@localhost:3000/api/users/2 | jq
+#curl  https://admin:admin@expmonitor.freeddns.org:3000/api/users/2 | jq
 
 # 2. create new team
 curl -X POST -H "Content-Type: application/json" -d '{
   "name": "ViewerTeam"
-}' http://admin:admin@localhost:3000/api/teams
+}' https://admin:admin@expmonitor.freeddns.org:3000/api/teams
 
-#curl  http://admin:admin@localhost:3000/api/teams/1 | jq
+#curl  https://admin:admin@expmonitor.freeddns.org:3000/api/teams/1 | jq
 
 # 3. associate user to team
 curl -X POST -H "Content-Type: application/json" -d '{
   "userId":2
-}' http://admin:admin@localhost:3000/api/teams/1/members
+}' https://admin:admin@expmonitor.freeddns.org:3000/api/teams/1/members
 
 # 4. change admin password
 curl -X PUT -H "Content-Type: application/json" -d "{
   \"oldPassword\": \"admin\",
   \"newPassword\": \"$ADMIN_PASSWORD\",
   \"confirmNew\": \"$ADMIN_PASSWORD\"
-}" http://admin:admin@localhost:3000/api/user/password
+}" https://admin:admin@expmonitor.freeddns.org:3000/api/user/password
