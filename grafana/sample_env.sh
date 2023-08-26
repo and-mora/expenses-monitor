ea1dcc513ab1ec2dee8fc1490c0c0088269be8da
Andrea Morabito <78792023+and-mora@users.noreply.github.com>
2023-08-26 16:41:52 +0200

    fix: [EM-80] parametrize grafana init script (#39)

---
+++ grafana/sample_env.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+### config user viewer only
+exports VIEWER_NAME=name_displayed
+exports VIEWER_EMAIL=user_email
+exports VIEWER_PASSWORD=user_password
+
+### config admin account password
+exports ADMIN_PASSWORD=admin_password
