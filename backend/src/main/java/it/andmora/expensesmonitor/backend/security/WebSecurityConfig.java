509a1b64c984a19783ca0fc6a20ace69046babb0
Andrea Morabito <andrea.morabito@pagopa.it>
2023-09-24 23:20:09 +0200

    add basic auth configuration

---
+++ backend/src/main/java/it/andmora/expensesmonitor/backend/security/WebSecurityConfig.java
@@ -0,0 +1,46 @@
+package it.andmora.expensesmonitor.backend.security;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class WebSecurityConfig {
+
+  @Value("basic-auth.username")
+  private String userDefault;
+  @Value("basic-auth.password")
+  private String password;
+
+  @Bean
+  public MapReactiveUserDetailsService userDetailsService() {
+    UserDetails user = User.withUsername(userDefault)
+        .password(password)
+        .roles("USER")
+        .build();
+
+    return new MapReactiveUserDetailsService(user);
+  }
+
+  @Bean
+  public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+    http
+        .authorizeExchange(exchanges -> exchanges
+            .anyExchange().authenticated()
+        )
+        .httpBasic(withDefaults())
+//        .formLogin(withDefaults())
+        .logout(withDefaults());
+
+    return http.build();
+  }
+}
