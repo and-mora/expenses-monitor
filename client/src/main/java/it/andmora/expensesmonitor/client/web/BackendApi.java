d86d47509ea6e833cb17a959befbe0f0d23b05c8
Andrea Morabito <78792023+and-mora@users.noreply.github.com>
2023-10-06 00:19:27 +0200

    fix: [EM-125] refactor client auth (#85)
    * remove spring security
    
    * add controller advice to manage exception due to unauthorized call
    
    * forward auth header to backend
    
    * bump version

--- client/src/main/java/it/andmora/expensesmonitor/client/web/BackendApi.java
+++ client/src/main/java/it/andmora/expensesmonitor/client/web/BackendApi.java
@@ -1,12 +1,16 @@
 package it.andmora.expensesmonitor.client.web;
 
+import feign.Headers;
+import feign.Param;
 import feign.RequestLine;
 import reactivefeign.spring.config.ReactiveFeignClient;
 import reactor.core.publisher.Mono;
 
 @ReactiveFeignClient(name = "backendApi")
+@Headers("Authorization: {authToken}")
 public interface BackendApi {
 
   @RequestLine("GET /greet")
-  Mono<String> getGreet();
+  Mono<String> getGreet(@Param("authToken") String authToken);
+
 }
--- client/src/main/java/it/andmora/expensesmonitor/client/web/HomepageController.java
+++ client/src/main/java/it/andmora/expensesmonitor/client/web/HomepageController.java
@@ -1,21 +1,21 @@
 package it.andmora.expensesmonitor.client.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
+@RequiredArgsConstructor
 public class HomepageController {
 
   private final BackendApi backendApi;
 
-  public HomepageController(BackendApi backendApi) {
-    this.backendApi = backendApi;
-  }
-
-  @GetMapping("/data")
-  Mono<String> data() {
-    return backendApi.getGreet();
+  @GetMapping("/greet")
+  Mono<String> greet(
+      @RequestHeader(defaultValue = "noAuth", name = "Authorization") String basicAuth) {
+    return backendApi.getGreet(basicAuth);
   }
 
   @GetMapping("/public/data")
