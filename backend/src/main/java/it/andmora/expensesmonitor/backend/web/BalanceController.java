a5355c21d67de5fdd9a4952ab61281f56cb00670
Andrea Morabito <78792023+and-mora@users.noreply.github.com>
2023-11-21 09:33:54 +0100

    fix: [EM-180] rename base path api (#142)

--- backend/src/main/java/it/andmora/expensesmonitor/backend/web/BalanceController.java
+++ backend/src/main/java/it/andmora/expensesmonitor/backend/web/BalanceController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import reactor.core.publisher.Mono;
 
-@RequestMapping("balance")
+@RequestMapping("api/balance")
 public interface BalanceController {
 
   @GetMapping
