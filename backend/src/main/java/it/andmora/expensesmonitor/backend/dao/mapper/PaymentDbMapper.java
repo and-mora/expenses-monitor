40f1deb36d31dda09d541e4bf207ea3e37e1be95
Andrea Morabito <andrea.morabito@pagopa.it>
2023-11-22 19:56:08 +0100

    fix mapping

--- backend/src/main/java/it/andmora/expensesmonitor/backend/dao/mapper/PaymentDbMapper.java
+++ backend/src/main/java/it/andmora/expensesmonitor/backend/dao/mapper/PaymentDbMapper.java
@@ -3,12 +3,10 @@ package it.andmora.expensesmonitor.backend.dao.mapper;
 import it.andmora.expensesmonitor.backend.dao.dbmodel.PaymentDbEntity;
 import it.andmora.expensesmonitor.backend.domain.model.Payment;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PaymentDbMapper {
 
-  @Mapping(ignore = true, target = "id")
   PaymentDbEntity domainToDbEntity(Payment payment);
 
   Payment dbEntityToDomain(PaymentDbEntity paymentDbEntity);
