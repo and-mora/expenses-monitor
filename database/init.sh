d4d76874e75f04152e29996c3161a65478e5cfe0
Andrea Morabito <andrea.morabito@pagopa.it>
2024-01-24 16:28:19 +0100

    update db script

--- database/init.sh
+++ database/init.sh
@@ -5,13 +5,13 @@ wget https://raw.githubusercontent.com/and-mora/expenses-monitor/master/database
 wget https://raw.githubusercontent.com/and-mora/expenses-monitor/master/database/init-system-users.sql
 
 # copy sql files into the container
-sudo docker cp schema.sql $(sudo docker ps -f name=postgres -q):schema.sql
-sudo docker cp init-system-users.sql $(sudo docker ps -f name=postgres -q):init-system-users.sql
+docker cp schema.sql $(docker ps -f name=postgres -q):schema.sql
+docker cp init-system-users.sql $(docker ps -f name=postgres -q):init-system-users.sql
 
 # run sql files
-sudo docker exec -t $(sudo docker ps -f name=postgres -q) psql -U postgres -d postgres -f schema.sql
-sudo docker exec -t $(sudo docker ps -f name=postgres -q) psql -U postgres -d postgres -f init-system-users.sql
+docker exec -t $(docker ps -f name=postgres -q) psql -U postgres -d postgres -f schema.sql
+docker exec -t $(docker ps -f name=postgres -q) psql -U postgres -d postgres -f init-system-users.sql
 
 # delete sql files from container
-sudo docker exec -t $(sudo docker ps -f name=postgres -q) rm schema.sql
-sudo docker exec -t $(sudo docker ps -f name=postgres -q) rm init-system-users.sql
+docker exec -t $(docker ps -f name=postgres -q) rm schema.sql
+docker exec -t $(docker ps -f name=postgres -q) rm init-system-users.sql
