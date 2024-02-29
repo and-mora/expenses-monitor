19d6469857e462964d214e5de37579c310df953b
Andrea Morabito <andrea.morabito@pagopa.it>
2024-02-29 15:27:50 +0100

    add auth guard

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  errorMessage: string = '';
 
+  constructor(private authService: AuthService) { }
+
+  login(): void {
+    if (this.authService.login(this.username, this.password)) {
+      // Redirect to home page or dashboard upon successful login
+      console.log('Login successful');
+    } else {
+      this.errorMessage = 'Invalid username or password';
+    }
+  }
 }
