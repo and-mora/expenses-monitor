1db4c2f2c7a665d513a293ea897a916d9c0db7f5
Andrea Morabito <andrea.morabito@pagopa.it>
2024-02-29 21:10:36 +0100

    implement logout plus optimizations

--- frontend/src/app/interfaces/header/header.component.ts
+++ frontend/src/app/interfaces/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  public isLoggedIn$: Observable<boolean>;
+
+  constructor(private router: Router, private authService: AuthService) {
+    this.isLoggedIn$ = this.authService.isLoggedIn$;
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 
 }
