a82ec482269d3b350089df1bc1c804a7693efbd9
Andrea Morabito <andrea.morabito@pagopa.it>
2024-03-01 18:36:37 +0100

    refactor guard with promise

--- frontend/src/app/guards/auth.guard.ts
+++ frontend/src/app/guards/auth.guard.ts
@@ -3,13 +3,5 @@ import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  if (inject(AuthService).isAuthenticated()) {
-    console.log("Guard: ok");
-    return true;
-  }
-
-  console.log("Guard: ko");
-  // navigate to login page as user is not authenticated
-  inject(Router).navigate(['/login']);
-  return false;
+  return inject(AuthService).isAuthenticated();
 };
