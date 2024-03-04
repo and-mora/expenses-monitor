af47759a9e4348ec3dfc96435f853e030a985eeb
Andrea Morabito <andrea.morabito@pagopa.it>
2024-03-04 23:56:01 +0100

    refactor header component

--- frontend/src/app/interfaces/header/header.component.ts
+++ frontend/src/app/interfaces/header/header.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
@@ -11,9 +13,10 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule]
+  imports: [AsyncPipe, NgIf, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule]
 })
 export class HeaderComponent {
+  @Input() inputSideNav!: MatSidenav;
   public isLoggedIn$: Observable<boolean>;
 
   constructor(private router: Router, private authService: AuthService) {
