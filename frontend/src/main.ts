bf54841fdf22767ea9b5c471000e322bb0e920bb
Andrea Morabito <andrea.morabito@pagopa.it>
2024-03-04 23:57:00 +0100

    refactor app component

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,9 @@
-
-
 import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { PreloadAllModules, provideRouter, withDebugTracing, withPreloading } from '@angular/router';
+import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
 import { ROUTES } from './app/app-routing';
 import { AppComponent } from './app/app.component';
 import { authInterceptor } from './app/interceptor/auth.interceptor';
@@ -17,7 +15,7 @@ bootstrapApplication(AppComponent, {
         provideAnimationsAsync(),
         provideHttpClient(withInterceptors([authInterceptor])),
         provideHttpClient(withInterceptorsFromDi()),
-        provideRouter(ROUTES, withPreloading(PreloadAllModules), withDebugTracing())
+        provideRouter(ROUTES, withPreloading(PreloadAllModules))
     ]
 })
     .catch(err => console.error(err));
