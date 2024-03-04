9a2bcec68a3809ab8b13f1a1f62a5f7cdbf594aa
Andrea Morabito <78792023+and-mora@users.noreply.github.com>
2024-03-04 23:51:47 +0100

    fix(frontend): [EM-233] convert application to standalone components (#193)

--- frontend/src/app/interfaces/header/header.component.ts
+++ frontend/src/app/interfaces/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../../services/auth.service';
@@ -6,7 +9,9 @@ import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule]
 })
 export class HeaderComponent {
   public isLoggedIn$: Observable<boolean>;
