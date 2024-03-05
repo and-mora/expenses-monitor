ca32fc9c2e571220c36fa3b15ff5aa2ec5872bfa
Andrea Morabito <andrea.morabito@pagopa.it>
2024-03-05 12:12:56 +0100

    add routing for the new component

--- frontend/src/app/app-routing.ts
+++ frontend/src/app/app-routing.ts
@@ -4,4 +4,5 @@ import { authGuard } from './guards/auth.guard';
 export const ROUTES: Route[] = [
   { path: '', canActivate: [authGuard], loadComponent: () => import('./interfaces/homepage/homepage.component').then(mod => mod.HomepageComponent) },
   { path: 'login', loadComponent: () => import('./interfaces/login/login.component').then(mod => mod.LoginComponent) },
+  { path: 'add-payment', canActivate: [authGuard], loadComponent: () => import('./interfaces/add-payment/add-payment.component').then(mod => mod.AddPaymentComponent) },
 ];
