35fc45a1a5a0a643b8ec703115f248c3eddbd6c4
Andrea Morabito <andrea.morabito@pagopa.it>
2024-09-11 11:47:40 +0200

    add skeleton

---
+++ backend_rust/src/main.rs
@@ -0,0 +1,25 @@
+use env_logger::Env;
+use sqlx::PgPool;
+use std::net::TcpListener;
+use expenses_monitor_be::configuration::get_configuration;
+use expenses_monitor_be::startup::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    // logger configuration
+    env_logger::Builder::from_env(Env::default().default_filter_or("debug")).init();
+
+    let configuration = get_configuration().expect("Failed to read configuration");
+
+    // tpc configuration
+    let address = format!("localhost:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+
+    // database configuration
+    let connection_string = configuration.database.connection_string();
+    let connection_pool = PgPool::connect(&connection_string)
+        .await
+        .expect("Failed to connect to database");
+
+    run(listener, connection_pool)?.await
+}
