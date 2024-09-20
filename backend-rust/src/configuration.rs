3dd5cd05d77262e90e68dac3adeeec6dd2ce3941
Andrea Morabito <andrea.morabito@pagopa.it>
2024-09-20 19:21:26 +0200

    fix application port property

--- backend-rust/src/configuration.rs
+++ backend-rust/src/configuration.rs
@@ -3,7 +3,12 @@ use serde::Deserialize;
 #[derive(Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings,
+}
+
+#[derive(Deserialize)]
+pub struct ApplicationSettings {
+    pub port: u16
 }
 
 #[derive(Deserialize)]
@@ -33,11 +38,29 @@ impl DatabaseSettings {
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let base_path = std::env::current_dir()
+        .expect("Failed to determine the current directory");
+    // let configuration_directory = base_path.join("configuration");
+
+    // Detect the running environment.
+    // Default to `local` if unspecified.
+    // let environment: Environment = std::env::var("APP_ENVIRONMENT")
+    //     .unwrap_or_else(|_| "local".into())
+    //     .try_into()
+    //     .expect("Failed to parse APP_ENVIRONMENT.");
+    // let environment_filename = format!("{:?}.yaml", environment);
+
     let settings = config::Config::builder()
+        // .add_source(config::File::from(configuration_directory.join("base.yaml")))
         .add_source(config::File::new(
             "configuration.yaml",
             config::FileFormat::Yaml,
         ))
+        .add_source(
+            config::Environment::with_prefix("APP")
+                .prefix_separator("_")
+                .separator("__")
+        )
         .build()?;
 
     settings.try_deserialize::<Settings>()
