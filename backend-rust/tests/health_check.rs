c01e436af3aa5c2f9b07e417707789d971372b64
Andrea Morabito <andrea.morabito@pagopa.it>
2024-09-27 22:25:52 +0200

    cargo fmt

--- backend-rust/tests/health_check.rs
+++ backend-rust/tests/health_check.rs
@@ -11,16 +11,29 @@ use uuid::Uuid;
 static TRACING: Lazy<()> = Lazy::new(|| {
     let default_filter_level = "info".to_string();
     let subscriber_name = "test".to_string();
-    let otlp_settings = TelemetrySettings { grpc_endpoint: String::from("http://localhost:4317"), service_name: String::from("be")};
+    let otlp_settings = TelemetrySettings {
+        grpc_endpoint: String::from("http://localhost:4317"),
+        service_name: String::from("be"),
+    };
     // We cannot assign the output of `get_subscriber` to a variable based on the
     // value TEST_LOG` because the sink is part of the type returned by
     // `get_subscriber`, therefore they are not the same type. We could work around
     // it, but this is the most straight-forward way of moving forward.
     if std::env::var("TEST_LOG").is_ok() {
-        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout, otlp_settings);
+        let subscriber = get_subscriber(
+            subscriber_name,
+            default_filter_level,
+            std::io::stdout,
+            otlp_settings,
+        );
         init_subscriber(subscriber);
     } else {
-        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink, otlp_settings);
+        let subscriber = get_subscriber(
+            subscriber_name,
+            default_filter_level,
+            std::io::sink,
+            otlp_settings,
+        );
         init_subscriber(subscriber);
     };
 });
