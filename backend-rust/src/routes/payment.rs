b5af2181d82074a99941393ca8833b1bff93f3b2
Andrea Morabito <78792023+and-mora@users.noreply.github.com>
2024-09-27 17:06:29 +0200

    feat(backend-rust): [EM-348] usage of tracing crate (#374)

--- backend-rust/src/routes/payment.rs
+++ backend-rust/src/routes/payment.rs
@@ -1,7 +1,8 @@
 use actix_web::{web, HttpResponse, Responder};
 use chrono::NaiveDateTime;
 use serde::Deserialize;
-use sqlx::PgPool;
+use sqlx::{Error, PgPool};
+use std::ops::Deref;
 use uuid::Uuid;
 
 #[derive(Deserialize)]
@@ -17,23 +18,33 @@ pub struct Payment {
     accounting_date: NaiveDateTime,
 }
 
+#[tracing::instrument(
+    name = "Creating a new payment",
+    skip(payload, connection_pool),
+    fields(
+        merchant_name = %payload.merchant_name,
+        category = %payload.category
+    )
+)]
 pub async fn create_payment(
     payload: web::Json<Payment>,
     connection_pool: web::Data<PgPool>,
 ) -> impl Responder {
-    let request_id = Uuid::new_v4();
-    tracing::info!(
-        "request_id {} - Adding '{}' '{}' as a new payment.",
-        request_id,
-        payload.merchant_name,
-        payload.category
-    );
-    tracing::info!(
-        "request_id {} - Saving new payment in the database",
-        request_id
-    );
+    match insert_payment(payload.deref(), connection_pool.deref()).await {
+        Ok(_) => HttpResponse::Ok().await,
+        Err(e) => {
+            tracing::error!("Failed to execute query: {:?}", e);
+            HttpResponse::InternalServerError().await
+        }
+    }
+}
 
-    match sqlx::query!(
+#[tracing::instrument(
+    name = "Inserting a new payment in the database",
+    skip(payload, connection_pool)
+)]
+async fn insert_payment(payload: &Payment, connection_pool: &PgPool) -> Result<(), Error> {
+    sqlx::query!(
         "insert into expenses.payments (category, description, merchant_name, accounting_date, amount) values ($1, $2, $3, $4, $5)",
         payload.category,
         payload.description,
@@ -41,54 +52,52 @@ pub async fn create_payment(
         payload.accounting_date,
         payload.amount_in_cents
     )
-        .execute(connection_pool.get_ref())
+        .execute(connection_pool)
         .await
-    {
-        Ok(_) => {
-            tracing::info!(
-                "request_id {} - New payment has been saved",
-                request_id
-            );
-            HttpResponse::Ok()
-        }
-        Err(e) => {
-            tracing::error!(
-                "request_id {} - Failed to execute query: {:?}",
-                request_id,
-                e
-            );
-            HttpResponse::InternalServerError()
-        }
-    }
+        .map_err(|e| {
+            tracing::error!("Failed to execute query: {:?}", e);
+            e
+            // Using the `?` operator to return early
+            // if the function failed, returning a sqlx::Error
+        })?;
+    Ok(())
 }
 
+#[tracing::instrument(
+    name = "Deleting a payment",
+    skip(path, connection_pool),
+    fields(
+        payment_id = %path.clone()
+    )
+)]
 pub async fn delete_payment(
     path: web::Path<Uuid>,
     connection_pool: web::Data<PgPool>,
 ) -> impl Responder {
-    let request_id = Uuid::new_v4();
     let payment_id = path.into_inner();
-    tracing::info!(
-        "request_id {} - Deleting payment '{}'.",
-        request_id,
-        payment_id
-    );
 
-    match sqlx::query!("delete from expenses.payments where id = $1", payment_id)
-        .execute(connection_pool.get_ref())
-        .await
-    {
-        Ok(_) => {
-            tracing::info!("request_id {} - payment has been deleted", request_id);
-            HttpResponse::Ok()
-        }
+    match delete_payment_query(connection_pool.get_ref(), payment_id).await {
+        Ok(_) => HttpResponse::Ok().await,
         Err(e) => {
-            tracing::error!(
-                "request_id {} - Failed to execute query: {:?}",
-                request_id,
-                e
-            );
-            HttpResponse::InternalServerError()
+            tracing::error!("Failed to execute query: {:?}", e);
+            HttpResponse::InternalServerError().await
         }
     }
+}
+
+#[tracing::instrument(
+    name = "Deleting a payment in the database",
+    skip(payment_id, connection_pool)
+)]
+async fn delete_payment_query(connection_pool: &PgPool, payment_id: Uuid) -> Result<(), Error> {
+    sqlx::query!("delete from expenses.payments where id = $1", payment_id)
+        .execute(connection_pool)
+        .await
+        .map_err(|e| {
+            tracing::error!("Failed to execute query: {:?}", e);
+            e
+            // Using the `?` operator to return early
+            // if the function failed, returning a sqlx::Error
+        })?;
+    Ok(())
 }
--- backend-rust/src/startup.rs
+++ backend-rust/src/startup.rs
@@ -1,15 +1,15 @@
 use crate::routes::{create_payment, delete_payment, health_check};
 use actix_web::dev::Server;
-use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, connection_pool: PgPool) -> Result<Server, std::io::Error> {
     let connection_pool = web::Data::new(connection_pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health", web::get().to(health_check))
             .route("/api/payment", web::post().to(create_payment))
             .route("/api/payment/{id}", web::delete().to(delete_payment))
