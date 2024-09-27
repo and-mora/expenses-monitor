e7c4b5f823a6a31f54225fffdb5776f855b37556
Andrea Morabito <andrea.morabito@pagopa.it>
2024-09-27 14:50:09 +0200

    add tracing crate

--- backend-rust/src/main.rs
+++ backend-rust/src/main.rs
@@ -1,13 +1,15 @@
-use env_logger::Env;
 use expenses_monitor_be::configuration::get_configuration;
 use expenses_monitor_be::startup::run;
+use expenses_monitor_be::telemetry::{get_subscriber, init_subscriber};
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     // logger configuration
-    env_logger::Builder::from_env(Env::default().default_filter_or("debug")).init();
+    let subscriber = get_subscriber("expenses-monitor-be".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
 
@@ -17,7 +19,7 @@ async fn main() -> Result<(), std::io::Error> {
 
     // database configuration
     let connection_string = configuration.database.connection_string();
-    let connection_pool = PgPool::connect(&connection_string)
+    let connection_pool = PgPool::connect(&connection_string.expose_secret())
         .await
         .expect("Failed to connect to database");
 
