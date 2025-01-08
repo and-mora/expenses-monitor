c551f0d0aef318fbb5eb9c3d825ae1878f1695d4
Andrea Morabito <andrea.morabito@pagopa.it>
2025-01-08 12:21:54 +0100

    revert schema change

--- database/k8s/schema.sql
+++ database/k8s/schema.sql
@@ -45,9 +45,3 @@ CREATE TABLE expenses.payment_tags (
 );
 
 alter database "expenses-monitor" set search_path = expenses;
-
-CREATE TABLE expenses.wallets (
-  id uuid DEFAULT gen_random_uuid(),
-  name varchar NULL,
-  CONSTRAINT wallets_pk PRIMARY KEY (id)
-);
