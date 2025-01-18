8915d99035bb8905ab16480bf1a0a8a15f2b3069
Andrea Morabito <andrea.morabito@pagopa.it>
2025-01-18 12:24:04 +0100

    change mapping from wallet name to id in saving payment

--- backend/src/main/java/it/andmora/expensesmonitor/backend/dao/dbmodel/PaymentDbEntity.java
+++ backend/src/main/java/it/andmora/expensesmonitor/backend/dao/dbmodel/PaymentDbEntity.java
@@ -23,5 +23,5 @@ public class PaymentDbEntity {
   @Column("accounting_date")
   private LocalDateTime accountingDate;
   private String category;
-  private String wallet;
+  private UUID wallet;
 }
