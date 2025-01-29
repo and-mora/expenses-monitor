010279ff1498270aa7c97145bb12e13a4c1223e1
Andrea Morabito <andrea.morabito@pagopa.it>
2025-01-29 20:37:03 +0100

    add database migration

---
+++ backend/src/main/resources/db/migrations/006_tags_feature.sql
@@ -0,0 +1,18 @@
+DROP TABLE expenses.payment_tags;
+DROP TABLE expenses.tags;
+
+CREATE TABLE expenses.tags (
+  id uuid DEFAULT gen_random_uuid(),
+  "key" varchar NOT NULL,
+	"value" varchar NOT NULL,
+	CONSTRAINT tags_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE expenses.payment_tags (
+  id uuid DEFAULT gen_random_uuid(),
+	"key" varchar NOT NULL,
+	"value" varchar NOT NULL,
+	payment_id uuid NOT NULL,
+	CONSTRAINT paymenttag_pk PRIMARY KEY (id),
+	CONSTRAINT paymenttag_fk_1 FOREIGN KEY (payment_id) REFERENCES expenses.payments(id)
+);
