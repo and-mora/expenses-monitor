672969912337ebe9bc2c3019525f01cd61f27293
and-mora <andrea.morabito@pagopa.it>
2025-03-25 19:51:39 +0100

    update header page

--- frontend/src/app/interfaces/header/header.component.ts
+++ frontend/src/app/interfaces/header/header.component.ts
@@ -1,36 +1,49 @@
-import { AsyncPipe } from '@angular/common';
-import { Component, Input, inject } from '@angular/core';
+import { Component, Input, effect, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { Router, RouterLink } from '@angular/router';
-import { Observable } from 'rxjs';
+import { KEYCLOAK_EVENT_SIGNAL, KeycloakEventType, ReadyArgs, typeEventArgs } from 'keycloak-angular';
+import Keycloak from 'keycloak-js';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html',
-    styleUrl: './header.component.css',
-    imports: [RouterLink, AsyncPipe, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule]
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+  imports: [RouterLink, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule]
 })
 export class HeaderComponent {
+  authenticated = false;
+  keycloakStatus: string | undefined;
+
   private router = inject(Router);
+  private readonly keycloak = inject(Keycloak);
   private authService = inject(AuthService);
+  private readonly keycloakSignal = inject(KEYCLOAK_EVENT_SIGNAL);
 
   @Input() inputSideNav!: MatSidenav;
-  public isLoggedIn$: Observable<boolean>;
 
   constructor() {
-    this.isLoggedIn$ = this.authService.isLoggedIn$;
+    effect(() => {
+      const keycloakEvent = this.keycloakSignal();
+
+      this.keycloakStatus = keycloakEvent.type;
+
+      if (keycloakEvent.type === KeycloakEventType.Ready) {
+        this.authenticated = typeEventArgs<ReadyArgs>(keycloakEvent.args);
+      }
+
+      if (keycloakEvent.type === KeycloakEventType.AuthLogout) {
+        this.authenticated = false;
+      }
+      console.log("authenticated?", this.authenticated);
+    });
   }
 
-  logout(): void {
-    this.authService.logout().subscribe({
-      next: () => {
-        this.router.navigate(['/login']);
-      }
-    });
+  logout() {
+    this.keycloak.logout();
   }
 
 }
