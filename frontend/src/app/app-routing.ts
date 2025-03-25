2efbc997f953b0f13810c96353161578d91c2093
and-mora <andrea.morabito@pagopa.it>
2025-03-25 19:44:08 +0100

    add routing

--- frontend/src/app/app-routing.ts
+++ frontend/src/app/app-routing.ts
@@ -1,10 +1,10 @@
 import { Route } from '@angular/router';
-import { authGuard } from './guards/auth.guard';
+import { canActivateAuthRole } from './guards/auth.guard';
 
 export const ROUTES: Route[] = [
-  { path: '', canActivate: [authGuard], loadComponent: () => import('./interfaces/homepage/homepage.component').then(mod => mod.HomepageComponent) },
+  { path: '', canActivate: [canActivateAuthRole], loadComponent: () => import('./interfaces/homepage/homepage.component').then(mod => mod.HomepageComponent) },
   { path: 'login', loadComponent: () => import('./interfaces/login/login.component').then(mod => mod.LoginComponent) },
-  { path: 'add-payment', canActivate: [authGuard], loadComponent: () => import('./interfaces/add-payment/add-payment.component').then(mod => mod.AddPaymentComponent) },
-  { path: 'add-wallet', canActivate: [authGuard], loadComponent: () => import('./interfaces/add-wallet/add-wallet.component').then(mod => mod.AddWalletComponent) },
+  { path: 'add-payment', canActivate: [canActivateAuthRole], loadComponent: () => import('./interfaces/add-payment/add-payment.component').then(mod => mod.AddPaymentComponent) },
+  { path: 'add-wallet', canActivate: [canActivateAuthRole], loadComponent: () => import('./interfaces/add-wallet/add-wallet.component').then(mod => mod.AddWalletComponent) },
   { path: 'error', loadComponent: () => import('./interfaces/page-error/page-error.component').then(mod => mod.PageErrorComponent) },
 ];
