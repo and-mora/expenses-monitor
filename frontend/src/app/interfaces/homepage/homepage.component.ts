622c4facdb684b721651b26f50a5e66a639f2778
and-mora <andrea.morabito@pagopa.it>
2025-06-07 12:02:19 +0200

    add skeleton homepage

--- frontend/src/app/interfaces/homepage/homepage.component.ts
+++ frontend/src/app/interfaces/homepage/homepage.component.ts
@@ -1,15 +1,22 @@
 import { Component, inject } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { AuthService } from '../../services/auth.service';
+import { PaymentDto } from '../../model/payment';
+import { ApiService } from '../../services/api.service';
+import { CommonModule } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css',
-  standalone: true
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatListModule]
 })
 export class HomepageComponent {
-  private authService = inject(AuthService);
+  latestTransactions: PaymentDto[] = [];
 
+  constructor(private authService: AuthService, private apiService: ApiService) { }
 
   greet(): void {
     this.authService.checkSessionAlive();
